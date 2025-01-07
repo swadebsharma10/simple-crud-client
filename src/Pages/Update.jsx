e65e2c6eb6da36cd649f2e1c97933cc66aa5e8b5
SwadebDeb sharma <swadebriva2012@gmail.com>
2025-01-08 02:28:13 +0600

    implement put method

--- src/Pages/Update.jsx
+++ src/Pages/Update.jsx
@@ -3,34 +3,70 @@ import { useLoaderData } from "react-router-dom";
 const Update = () => {
   const loadedUser = useLoaderData();
 
-  const handleUpdate =e =>{
-      e.preventDefault();
-      const form = e.target;
-      const name = form.name.value;
-      const email = form.email.value;
-      const password = form.password.value;
-      const updateUser ={name, email, password};
-      console.log(updateUser)
-  }
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    const updateUser = { name, email, password };
+
+    fetch(`http://localhost:5000/users/${loadedUser._id}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(updateUser),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if(data.modifiedCount > 0){
+            alert('user updated Successfully');
+           
+        }
+      });
+  };
 
   return (
     <div>
-      <h2 className="text-center my-6 text-4xl font-bold">Update user Information of :{loadedUser.name}</h2>
+      <h2 className="text-center my-6 text-4xl font-bold">
+        Update user Information of :{loadedUser.name}
+      </h2>
       <div>
         <form onSubmit={handleUpdate} className="w-2/3 mx-auto">
           <label className="input input-bordered flex items-center gap-2 mb-2">
             Name:
-            <input type="text" name="name" defaultValue={loadedUser?.name} className="grow" placeholder="Daisy" />
+            <input
+              type="text"
+              name="name"
+              defaultValue={loadedUser?.name}
+              className="grow"
+              placeholder="Daisy"
+            />
           </label>
           <label className="input input-bordered flex items-center gap-2 mb-2">
             Email:
-            <input type="email" name="email" defaultValue={loadedUser?.email} className="grow" placeholder="Daisy@gmail" />
+            <input
+              type="email"
+              name="email"
+              defaultValue={loadedUser?.email}
+              className="grow"
+              placeholder="Daisy@gmail"
+            />
           </label>
           <label className="input input-bordered flex items-center gap-2 mb-2">
             Password:
-            <input type="text" name="password" defaultValue={loadedUser?.password} className="grow" placeholder="1235667" />
+            <input
+              type="text"
+              name="password"
+              defaultValue={loadedUser?.password}
+              className="grow"
+              placeholder="1235667"
+            />
           </label>
-          <button type="submit" className="btn btn-primary btn-sm btn-wide">Update user</button>
+          <button type="submit" className="btn btn-primary btn-sm btn-wide">
+            Update user
+          </button>
         </form>
       </div>
     </div>
